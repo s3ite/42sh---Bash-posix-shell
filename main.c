59ef6471441efe63201ba3249b68b69c92c07776
Julien Meyer <julien.meyer@epita.fr>
2023-01-07 01:49:23 +0100

    feat: add main.c

--- main.c
+++ main.c
@@ -1,6 +1,15 @@
 #include "parser/parser.h"
+#include "lexer/lexer.h"
 
-int main()
+int main(int argc, char **argv)
 {
-    
+
+    char *input = argv[1];
+    struct lexer *a=lexer_init(10, argv[1]);
+    a=lexer_load(argv[1],a);
+    parse( a);
+
+
+
+    return 1;
 }
