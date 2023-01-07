eb7ce8554617be6593f5bdac60e9194b3d442bdb
Jean Guerin <jean.guerin@epita.fr>
2023-01-07 20:47:11 +0100

    feat: add boolean.c/built_in.h

---
+++ built_in/boolean.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+#include "built_in.h"
+
+int my_true(void)
+{
+    return 0;
+}
+
+int my_false(void)
+{
+    return 1;
+}
---
+++ built_in/built_in.h
@@ -0,0 +1,7 @@
+#ifndef BUILT_IN_H
+#define BUILT_IN_H
+
+int my_true(void);
+int my_false(void);
+
+#endif
