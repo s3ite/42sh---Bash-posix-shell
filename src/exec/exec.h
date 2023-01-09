1a2c46c3c5f75289dbe054e995236d60c652f7e0
Julien Meyer <julien.meyer@epita.fr>
2023-01-09 17:35:30 +0100

    feat: multiple executions commands

--- src/exec/exec.h
+++ src/exec/exec.h
@@ -1,8 +1,17 @@
 #ifndef EXEX_H
 #define EXEX_H
 
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
+#include <sys/wait.h>
+
 #include "../ast/ast.h"
 #include "../parser/parser.h"
+#include "../lexer/lexer.h"
 
 int run_command(char **cmd);
-#endif
+int global_exec(struct parser *parser);
+
+#endif /*EXEC_H*/
--- src/parser/parser.c
+++ src/parser/parser.c
@@ -3,96 +3,6 @@
 #include "../exec/exec.h"
 
 
-struct ast_node *ast_list_init(void)
-{
-    struct ast_node *res = malloc(sizeof(struct ast_node));
-
-    res->ast = NULL;
-    res->next = NULL;
-
-    return res;
-}
-
-static void free_cmd(char **cmd)
-{
-    size_t index = 0;
-    while(cmd[index])
-    {
-        free(cmd[index]);
-        index++;
-    }
-    free(cmd);
-}
-
-char **to_command(struct dlist *prefix,struct dlist *values)
-{
-     if (!prefix || !values)
-        return NULL;
-
-    size_t size = dlist_size(prefix) + dlist_size(values);
-
-    struct dlist_item *tmp1 = prefix->head;
-    struct dlist_item *tmp2 = values->head;
-
-    char **cmd = malloc(sizeof(char *) * (size + 1));
-    size_t i = 0;
-
-    for ( i=0; i < size && tmp1; ++i)
-    {
-        cmd[i] = strdup(tmp1->value);
-        tmp1 = tmp1->next;
-    }
-
-    for (; i < size && tmp2; ++i)
-    {
-        cmd[i] = strdup(tmp2->value);
-        tmp2 = tmp2->next;
-    }
-
-    cmd[size] = NULL;
-    return cmd;
-
-}
-
-int simple_cmd_exec(struct ast *ast)
-{
-
-    struct simple_command_node *cmd_nbode = ast->node;
-   
-    struct dlist *prefix = cmd_nbode->prefix;
-    struct dlist *values = cmd_nbode->values;
-
-    //struct dlist_item *head = prefix->head; 
-    char **cmd = to_command(prefix, values);
-
-    int rc = run_command(cmd);
-    free_cmd(cmd);
-
-
-
-    return rc;
-}
-
-int global_exec(struct ast *node)
-{
-    if(node->node_type == SIMPLE_COMMAND)
-    {
-        simple_cmd_exec(node);
-    }
-    else 
-    {
-       // printf("%s\n", "NOT MATCH !!!!!==>");
-    }
-    return RC_SUCCESS;
-}
-
-
-
-// static void parser_destroy(struct parser *parser)
-// {
-//     (void) parser;
-// }
-
 
 
 /*
@@ -116,9 +26,8 @@ int parse(struct lexer *lexer)
     if(rc == RC_ERROR) //TODO: Free all structures to avoid memory leak. Then return error code.
         return RC_ERROR;
 
-    global_exec(parser->ast);
+    global_exec(parser);
 
-    printf("%s\n", "Hit");
     
     parser_destroy(parser);
 
