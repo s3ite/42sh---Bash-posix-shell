fc7ee703f65bce7ffe8623eb47219e60bf1b406f
Guy Noe DJOUFAING <guy-noe.djoufaing-nzumgueng@epita.fr>
2023-01-09 16:34:09 +0100

    oublie du dossier others

---
+++ src/others/tools.c
@@ -0,0 +1,14 @@
+#include "tools.h"
+
+
+char *strdup(const char *str)
+{
+size_t len;
+char *copy;
+ 
+len = strlen(str) + 1;
+if (!(copy = malloc(len)))
+return (NULL);
+memcpy(copy, str, len);
+return (copy);
+}
---
+++ src/others/tools.h
@@ -0,0 +1,9 @@
+#ifndef TOOLS_H
+#define TOOLS_H
+
+#include <stddef.h>
+#include <stdlib.h>
+#include <string.h>
+
+char *strdup(const char *str);
+#endif /* !TOOL_H */
