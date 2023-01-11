2aad010deec0ab5961ff06ee251223ad80473838
Jean Guerin <jean.guerin@epita.fr>
2023-01-11 15:24:59 +0100

    fix: parse_command_line/main.c

--- src/main.c
+++ src/main.c
@@ -1,41 +1,41 @@
-#include "parser/parser.h"
-#include "lexer/lexer.h"
 #include "exec/exec.h"
+#include "lexer/lexer.h"
 #include "parse_command_line/parse_command_line.h"
+#include "parser/parser.h"
 
 int main(int argc, char **argv)
 {
-    
-    char *input = parse_command_line(argc,argv);
-    if(!input)
+    char *input = parse_command_line(argc, argv);
+    if (!input)
     {
-        while(1)
+        while (1)
         {
-            char *str=malloc(1024);
-            printf( "42sh$ :");
-            fgets( str, 1024,stdin );
-            str[strlen(str)-1]='\0';
+            char *str = malloc(1024);
+            printf("42sh$ :");
+            fgets(str, 1024, stdin);
+            str[strlen(str) - 1] = '\0';
 
             struct lexer *lexer = lexer_init(10, input);
             lexer = lexer_load(input, lexer);
-            int rc = parse(lexer);
+            lexer_print(lexer);
+            // int rc = parse(lexer);
             lexer_destroy(lexer);
-            if(rc == RC_ERROR)
-                return RC_ERROR;
+            // if(rc == RC_ERROR)
+            // return RC_ERROR;
         }
     }
 
     struct lexer *lexer = lexer_init(10, input);
     lexer = lexer_load(input, lexer);
 
-    //representation of the ast
-    //print_ast(lexer);
+    // representation of the ast
+    // print_ast(lexer);
 
     int rc = parse(lexer);
-    
+
     lexer_destroy(lexer);
 
-    if(rc == RC_ERROR)
+    if (rc == RC_ERROR)
         return RC_ERROR;
 
     return 1;
--- src/parse_command_line/parse_command_line.c
+++ src/parse_command_line/parse_command_line.c
@@ -1,12 +1,12 @@
 #define _POSIX_C_SOURCE 2
 
+#include "parse_command_line.h"
+
 #include <stddef.h>
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
 #include <unistd.h>
-
-#include "parse_command_line.h"
 
 int get_file_size(FILE *f)
 {
@@ -18,75 +18,71 @@ int get_file_size(FILE *f)
 
 char *parse_command_line(int argc, char **argv)
 {
-    if(argc >= 3)
+    if (argc == 3)
     {
         int opt;
-        int l;
+        size_t l;
         char *a;
-        while((opt=getopt(argc, argv, "c:")) != -1)
+        while ((opt = getopt(argc, argv, "c:")) != -1)
         {
             switch (opt)
             {
-                case 'c':
-                    l=strlen(optarg);
-                    a=malloc(l+1);
-                    strcpy(a,optarg);
-                    return a;
+            case 'c':
+                l = strlen(optarg);
+                a = malloc(l + 1);
+                strcpy(a, optarg);
+                return a;
             }
         }
     }
-    if(argc == 2)
+    if (argc == 2)
     {
         FILE *f;
-        if((f = fopen(argv[optind],"r")))
+        if ((f = fopen(argv[optind], "r")))
         {
-            int l = get_file_size(f);
-            char* buff=malloc(l+1);
-            if(!buff)
+            size_t l = get_file_size(f);
+            char *buff = malloc(l + 1);
+            if (!buff)
             {
                 return NULL;
             }
-            fread(buff,l,1,f);
-            buff[l]='\0';
+            fread(buff, l, 1, f);
+            buff[l] = '\0';
             fclose(f);
             return buff;
         }
     }
-    if(argc == 1)
+    if (argc == 1)
     {
-        if((isatty(STDIN_FILENO)))
+        if ((isatty(STDIN_FILENO)))
         {
             return NULL;
         }
         int l = 1024;
-        size_t red=0;
-        char* buff=malloc(l+1);
-        if(!buff)
+        size_t red = 0;
+        char *buff = malloc(l + 1);
+        if (!buff)
         {
             return NULL;
         }
-        size_t i = fread(buff,sizeof(char),l,stdin);
-      //  printf("i = %d   ",i);
-        red+=i;
-        if(red==0)
+        size_t i = fread(buff, sizeof(char), l, stdin);
+        red += i;
+        if (red == 0)
         {
             return NULL;
         }
-        while(i == 1024)
+        while (i == 1024)
         {
             l += 1024;
-            buff = realloc(buff,l+1);
-            i = fread(buff + red,sizeof(char),1024,stdin);
-     //       printf("i = %d   ",i);
+            buff = realloc(buff, l + 1);
+            i = fread(buff + red, sizeof(char), 1024, stdin);
             red += i;
-     //       printf("red = %d   ",red);
         }
         buff[red] = '\0';
         return buff;
     }
     return NULL;
 }
-
 /*int main (int argc, char **argv)
 {
     char *res=parse_command_line(argc,argv);
