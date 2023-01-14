cd6880f59d5085dc60a4c936f008440a998a0e7d
Julien Meyer <julien.meyer@epita.fr>
2023-01-14 18:08:29 +0100

    fix: while & until loop

--- src/parser/parse_command.c
+++ src/parser/parse_command.c
@@ -10,7 +10,13 @@ struct ast *parse_command(struct lexer *lexer, struct parser *parser)
 {
     struct ast *res = NULL;
     struct token *token = lexer_peek(lexer);
-    if (token->type == TOKEN_IF || token->type == TOKEN_WHILE)
+    if (token->type == TOKEN_IF)
+    {
+        res = parse_shell_command(lexer, parser);
+        if (!res)
+            exit(999);
+    }
+    else if (token->type == TOKEN_WHILE || token->type == TOKEN_UNTIL)
     {
         res = parse_shell_command(lexer, parser);
         if (!res)
